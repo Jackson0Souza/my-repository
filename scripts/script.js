9c47caa2b6721b465c21458db2c957cd1cdae6d9
kaique jackson <kaiquejackson09@gmail.com>
2025-01-06 15:22:00 -0300

    estrutura de arquivos

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const observer = new IntersectionObserver((entries, observer) => {
+entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        entry.target.classList.add("fade-in");
+    } else {
+        entry.target.classList.remove("fade-in"); 
+    }
+});
+}, { threshold: 0.20 }); 
+
+const fadeElements = document.querySelectorAll('.hero, .about-me, .card, .my-tech, .projetos');
+
+fadeElements.forEach(element => {
+observer.observe(element);
+});
+
+const sections = document.querySelectorAll('section');
+const menuItems = document.querySelectorAll('nav ul li');
+
+const sectionObserver = new IntersectionObserver((entries, observer) => {
+entries.forEach(entry => {
+const menuItem = document.querySelector(`nav ul li a[href="#${entry.target.id}"]`);
+if (entry.isIntersecting) {
+menuItem.parentElement.classList.add('active'); 
+} else {
+menuItem.parentElement.classList.remove('active');  
+}
+});
+}, { threshold: 0.20 });  
+
+sections.forEach(section => {
+sectionObserver.observe(section); 
+});
